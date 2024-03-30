0c4ed23a5286547ace287bb78db326f27af3dbab
tylerb1 <tberb1290@gmail.com>
2024-03-30 10:48:57 -0500

    sampleTree

---
+++ src/lib/sampleTree.js
@@ -0,0 +1,30 @@
+export const sampleTree = {
+  name: "root",
+  children: [
+    {
+      name: "Child 1",
+      children: [
+        {
+          name: "Child 1.1",
+          value: 1000
+        },
+        {
+          name: "Child 1.2",
+          value: 500
+        },
+        {
+          name: "Child 1.3",
+          value: 250
+        },
+      ]
+    },
+    {
+      name: "Child2",
+      value: 800
+    },
+    {
+      name: "Child3",
+      value: 600
+    },
+  ]
+}
